a8a2b7cec4ae38d40932aeba9b436f1891912cf6
SantiBass <sibarraryan@gmail.com>
2021-11-18 09:26:52 -0800

    fist-commit-for-practice-API

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var chicagoInstitute = 'https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,main_reference_number=3'
+var chicagoInstitute = 'https://api.artic.edu/api/v1/artworks/51349/?fields=id,title,artist_display,date_display,main_reference_number=3'
 //console.log(chicagoInstitute)
 fetch(chicagoInstitute)
 .then(function(response){
@@ -6,24 +6,24 @@ fetch(chicagoInstitute)
 })  .then(function (data) {
 
     console.log(data)
+    console.log(data.config.iiif_url)
+
     // console.log(data.data[0].date_display)
     // console.log(data.data[0].title)
     // console.log(data.data[0].artist_display)
-
-
+    var iiifUrl = data.config.iiif_url
+var image_id = data.data.id
     var art =  'https://www.artic.edu/iiif/2/1adf2696-8489-499b-cad2-821d7fde4b33/full/843,/0/default.jpg'
 console.log(art)
 
 fetch(art)
-.then(function(response2){
-return response2.json()
-}).then(function(data2){
-    
-//  console.log(data2.config.iiif_url)
+ .then(function(response2){
+ return response2.json()
+ }).then(function(data2){
+  console.log(data2.config.iiif_url)
  
-})
+ })
    
-    console.log()
     console.log()
     console.log()
     console.log()
